9d082271a45ccf3a6458890a178b499559fde97a
TLHY <54972550+TLHY@users.noreply.github.com>
2022-08-27 03:53:05 +0900

    Update GPT_API.py

--- GPT_API.py
+++ GPT_API.py
@@ -47,4 +47,4 @@ def main():
     return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(host='https://main-test-api-tlhy.endpoint.ainize.ai', debug=True,port=80)
+    app.run(host='0.0.0.0', debug=True,port=80)
