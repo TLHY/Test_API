2a71d5c04730664c7172549795a0e2781047999b
TLHY <54972550+TLHY@users.noreply.github.com>
2022-08-27 20:31:24 +0900

    Update GPT_API.py

--- GPT_API.py
+++ GPT_API.py
@@ -2,10 +2,11 @@ from flask import Flask,request,jsonify,render_template
 from transformers import AutoTokenizer,AutoModelForCausalLM
 import torch
 from waitress import serve
-import time
-app=Flask(__name__)
 
+app=Flask(__name__)
 print("model loading...")
+
+#Load Model
 
 tokenizer=AutoTokenizer.from_pretrained('./persona_text')
 model=AutoModelForCausalLM.from_pretrained('./persona_text')
@@ -13,10 +14,12 @@ model=AutoModelForCausalLM.from_pretrained('./persona_text')
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
+#Make user input as Model input
 def text_maker(text):
+    text=text+'\n'
+    print("text making..")
     try: 
-        length=30
-        text="Main : "+text
+        length=100
         name_input=tokenizer.encode(text,return_tensors='pt')
         name_input = name_input.to(device)
         min_length = len(name_input.tolist()[0])
@@ -35,16 +38,21 @@ def text_maker(text):
     except Exception as e:
         print('Error occur in script generating!', e)
 
+#Generate text from user input
 @app.route('/gen', methods=['GET','POST'])
 def generate():
+    print("generating")
     if request.method == 'POST':
-        text=request.form.get('username',"No Input")
-        return render_template('main.html',username=str(text_maker(text)))
+        text=request.form.get('myText',"No Input")
+        output=str(text_maker(text)).replace(str(text),'')
+        print(output)
+        return render_template('main.html',Main_Text="Main: "+text,NPC_Text=output+"\n")
     return render_template('main.html')
 
 @app.route('/')
 def main():
+    print("service started")
     return render_template('main.html')
 
 if __name__ == '__main__':
-     serve(app, host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=80)
